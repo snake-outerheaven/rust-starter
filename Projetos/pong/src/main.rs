5d767c08304a6efd0f3156b6a2e26af9eb97a9ca
neo-dev <sei133dirsoa@gmail.com>
2025-08-26 14:58:09 -0300

    Mais alguns utilitários, e exemplos de uso

--- Projetos/pong/src/main.rs
+++ Projetos/pong/src/main.rs
@@ -1,11 +1,14 @@
 mod utils;
-use std::{thread::sleep, time::Duration};
-use utils::limpar_tela;
 
 fn main() {
     println!("Oi! Eu sou um Rustáceo!");
-    sleep(Duration::from_millis(750));
-    limpar_tela();
-    sleep(Duration::from_millis(750));
+    utils::wait(750);
+    utils::limpar_tela();
+    utils::wait(750);
     println!("Ei! Eu continuo sendo um Rustáceo!");
+    utils::wait(750);
+    println!("Ok, vamos testar as novas ferramentas!");
+    let x: (u32, u32) = utils::xy_generator(10, 10);
+    let (objx, objy) = x;
+    println!("{objx}, {objy}");
 }
--- Projetos/pong/src/utils.rs
+++ Projetos/pong/src/utils.rs
@@ -1,12 +1,35 @@
-use std::process::Command;
-// forma bonita de chamar variáveis
+use std::{
+    io::{self, Write},
+    process::Command,
+    thread::sleep,
+    time::Duration,
+};
 
+use rand::prelude::*;
+
+/// função dedicada
 pub fn limpar_tela() {
-    let cmd: &'static str = if cfg!(windows) { "cls" } else { "clear" };
+    let cmd: &str = if cfg!(windows) { "cls" } else { "clear" };
     // caso houvesse outros sistemas operacionais, seria um else if cfg!(unix)
     // para linux, mac e outros sistemas baseados em unix.
 
     if Command::new(cmd).status().is_err() {
-        println!("\x1B[2J\x1B[H");
+        print!("\x1B[2J\x1B[H");
+        if io::stdout().flush().is_err() {
+            eprintln!("Falha ao limpar tela, com ANSI escape...");
+        }
     }
+}
+
+pub fn wait(ms: u64) {
+    sleep(Duration::from_millis(ms));
+}
+
+pub fn xy_generator(width: u32, height: u32) -> (u32, u32) {
+    let mut rng = rand::rng();
+
+    let objx = rng.random_range(0..=width);
+    let objy = rng.random_range(0..=height);
+
+    (objx, objy)
 }
