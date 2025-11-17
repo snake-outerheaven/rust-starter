4664a47ba25b4f33d750968ef3fd66a35817f115
neo-dev <sei133dirsoa@gmail.com>
2025-11-17 11:33:09 -0300

    Rush 0.0.2V

--- Projetos/rush/src/main.rs
+++ Projetos/rush/src/main.rs
@@ -1,13 +1,21 @@
 use std::{
+    env::{current_dir, set_current_dir, var},
     io::{Write, stdin, stdout},
     process::Command,
 };
 
+use gethostname::gethostname;
+
 fn main() {
     loop {
-        print!("ferris@rusty$: ");
-        stdout().flush().expect("f rsh.");
+        let user: String = var("USER").unwrap_or_else(|_| "user".to_string());
+        let home: String = var("HOME").expect("Couldnt get home variable");
+        let hostname: String = gethostname().to_string_lossy().to_string();
+        let cwd: String = current_dir().unwrap().display().to_string();
+        let prompt: String = format!("{}@{}: {}! ", user, hostname, cwd);
         let mut input: String = String::new();
+        print!("{}", &prompt);
+        stdout().flush().expect("f rush.");
         stdin().read_line(&mut input).expect("f rsh");
 
         let tokens: Vec<&str> = input.trim().split_whitespace().collect();
@@ -18,6 +26,12 @@ fn main() {
 
         match tokens[0] {
             "exit" => break,
+            "cd" => {
+                let path: &str = tokens.get(1).map(|s| *s).unwrap_or(&home);
+                if let Err(e) = set_current_dir(&path) {
+                    eprintln!("cd: {}", e);
+                }
+            }
             cmd => {
                 Command::new(cmd).args(&tokens[1..]).status().ok();
             }
